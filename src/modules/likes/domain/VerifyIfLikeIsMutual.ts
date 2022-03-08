418013412182e98f487e142adcf8c2732ad3743a
Alfredo Villegas <alfreddovillegas@gmail.com>
2022-03-08 16:27:53 -0400

    rename domain event of likes Mutual

--- src/modules/likes/domain/VerifyIfLikeIsMutual.ts
+++ src/modules/likes/domain/VerifyIfLikeIsMutual.ts
@@ -1,14 +1,14 @@
+import { LikesMutualArchievedDomainEvent } from './LikesMutualArchievedDomainEvent';
 import { LikesRepository } from './LikesRepository';
-import { LikingIsMutualDomainEvent } from './LikingIsMutualDomainEvent';
 
 export class VerifyIfLikesIsMutual {
   constructor(private repository: LikesRepository) {}
 
-  public async execute(fromUser: string, whitUser: string): Promise<LikingIsMutualDomainEvent | null> {
+  public async execute(fromUser: string, whitUser: string): Promise<LikesMutualArchievedDomainEvent | null> {
     const likes = await this.repository.findLikeBetween(fromUser, whitUser);
     if (!likes) {
       return null;
     }
-    return new LikingIsMutualDomainEvent([fromUser, whitUser]);
+    return new LikesMutualArchievedDomainEvent([fromUser, whitUser]);
   }
 }
