5644a048f27696c3c866bf92f53b5d1b23fdcc87
Alfredo Villegas <alfreddovillegas@gmail.com>
2022-03-08 16:23:16 -0400

    rename domain event of likes Mutual

---
+++ src/modules/likes/domain/LikesMutualArchievedDomainEvent.ts
@@ -0,0 +1,10 @@
+import { DomainEvent } from '../../shared/DomainEvent';
+
+export class LikesMutualArchievedDomainEvent extends DomainEvent {
+  static EVENT_NAME = 'likes.mutual.archieved';
+  readonly couple: string[];
+  constructor(couple: string[]) {
+    super(LikesMutualArchievedDomainEvent.EVENT_NAME);
+    this.couple = couple;
+  }
+}
